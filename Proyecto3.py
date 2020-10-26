c05db2687bcd0112bcdb497e9179fd6d3dd2b5cd
camilavillalobos1 <civillalobos@miuandes.cl>
2020-10-26 13:19:10 -0300

    Arreglo list corregido

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,4 +1,3 @@
-
 try:
     file = open("p3F_1.ass", 'r') #Abre archivo de texto con assembly
 except IOError:
@@ -29,111 +28,29 @@ notDoubles = ["MOV", "CMP"]
 numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","#00","#01","#03","#04","#05","#06","#07","#08","#09",
            "#0A","#0B","#0C","#0D","#0E","#0F"]
 listaMov = ["(A),B", "A,(A)", "A,A", "B,B"]
+listaC = ["A,(B)", "B,(B)", "B,(A)"]
 
 
-binario = {'MOV A,B' : '0000000',
-                    'MOV B,A':'0000001',
-                    'ADD A,B':'0000100',
-                    'ADD B,A':'0000101',
-                    'SUB A,B':'0001000',
-                    'SUB B,A':'0001001',
-                    'AND A,B':'0001100',
-                    'AND B,A':'0001101',
-                    'OR A,B':'0010000',
-                    'OR B,A':'0010001',
-                    'NOT A,A':'0010100',
-                    'NOT A,B':'0010101',
-                    'NOT B,A':'0010110',
-                    'NOT B,B':'0010111',
-                    'XOR A,A':'0011000',
-                    'XOR B,A':'0011001',
-                    'SHL A,A':'0011100',
-                    'SHL A,B':'0011101',
-                    'SHL B,A':'0011110',
-                    'SHL B,B':'0011111',
-                    'SHR A,A':'0100000',
-                    'SHR A,B':'0100001',
-                    'SHR B,A':'0100010',
-                    'SHR B,B':'0100011',
-                    'INC B':'0100100',
-                    'CMP A,B':'1001101',
-                    'MOV A,(B)':'0101001',
-                    'MOV B,(B)':'0101010',
-                    'MOV (B),A':'0101011',
-                    'ADD A,(B)':'0101110',
-                    'SUB A,(B)':'0110010',
-                    'AND A,(B)':'0110110',
-                    'OR A,(B)':'0111010',
-                    'NOT (B)':'0111110',
-                    'XOR A,(B)':'1000001',
-                    'SHL (B)':'1000101',
-                    'SHR (B)':'1001000',
-                    'CMP A,(B)':'1010010',
-                    'INC (B)':'1001010',
-                    'RST (B)':'1001100'
+binario = {'MOV A,B' : '0000000','MOV B,A':'0000001','ADD A,B':'0000100','ADD B,A':'0000101','SUB A,B':'0001000','SUB B,A':'0001001','AND A,B':'0001100','AND B,A':'0001101',
+                    'OR A,B':'0010000','OR B,A':'0010001','NOT A,A':'0010100','NOT A,B':'0010101','NOT B,A':'0010110','NOT B,B':'0010111','XOR A,A':'0011000','XOR B,A':'0011001',
+                    'SHL A,A':'0011100','SHL A,B':'0011101','SHL B,A':'0011110','SHL B,B':'0011111','SHR A,A':'0100000','SHR A,B':'0100001','SHR B,A':'0100010','SHR B,B':'0100011',
+                    'INC B':'0100100','CMP A,B':'1001101','MOV A,(B)':'0101001','MOV B,(B)':'0101010','MOV (B),A':'0101011','ADD A,(B)':'0101110','SUB A,(B)':'0110010','AND A,(B)':'0110110',
+                    'OR A,(B)':'0111010','NOT (B)':'0111110','XOR A,(B)':'1000001','SHL (B)':'1000101','SHR (B)':'1001000','CMP A,(B)':'1010010','INC (B)':'1001010','RST (B)':'1001100'
                     }
 
-binario_literal = {'MOV A':'0000010',
-                    'MOV B':'0000011',
-                    'ADD A':'0000110',
-                    'ADD B':'0000111',
-                    'SUB A':'0001010',
-                    'SUB B':'0001011',
-                    'AND A':'0001110',
-                    'AND B':'0001111',
-                    'OR A':'0010010',
-                    'OR B':'0010011',
-                    'XOR A':'0011010',
-                    'XOR B':'0011011',
-                    'CMP A':'1001110',
-                    'CMP B':'1001111'
+binario_literal = {'MOV A':'0000010','MOV B':'0000011','ADD A':'0000110','ADD B':'0000111','SUB A':'0001010','SUB B':'0001011','AND A':'0001110','AND B':'0001111',
+                    'OR A':'0010010','OR B':'0010011','XOR A':'0011010','XOR B':'0011011','CMP A':'1001110','CMP B':'1001111'
                     }
 
-binario_direccionamiento = {'MOV A':'0100101',
-                            'MOV B':'0100110',
-                            'MOV (),A':'0100111',
-                            'MOV (),B':'0101000',
-                            'ADD A':'0101100',
-                            'ADD B':'0101101',
-                            'ADD':'0101111',
-                            'SUB A':'0110000',
-                            'SUB B':'0110001',
-                            'SUB':'0110011',
-                            'AND A':'0110100',
-                            'AND B':'0110101',
-                            'AND':'0110111',
-                            'OR A':'0111000',
-                            'OR B':'0111001',
-                            'OR':'0111011',
-                            'NOT (),A':'0111100',
-                            'NOT (),B':'0111101',
-                            'XOR A':'0111111',
-                            'XOR B':'1000000',
-                            'XOR ()':'1000010',
-                            'SHL (),A':'1000011',
-                            'SHL (),B':'1000100',
-                            'SHR (),A':'1000110',
-                            'SHR (),B':'1000111',
-                            'INC ()':'1001001',
-                            'RST':'1001011',
-                            'CMP A':'1010000',
-                            'CMP B':'1010001',
+binario_direccionamiento = {'MOV A':'0100101','MOV B':'0100110','MOV (),A':'0100111','MOV (),B':'0101000','ADD A':'0101100','ADD B':'0101101','ADD':'0101111',
+                            'SUB A':'0110000','SUB B':'0110001','SUB':'0110011','AND A':'0110100','AND B':'0110101','AND':'0110111','OR A':'0111000','OR B':'0111001',
+                            'OR':'0111011','NOT (),A':'0111100','NOT (),B':'0111101','XOR A':'0111111','XOR B':'1000000','XOR ()':'1000010','SHL (),A':'1000011',
+                            'SHL (),B':'1000100','SHR (),A':'1000110','SHR (),B':'1000111','INC ()':'1001001','RST':'1001011','CMP A':'1010000','CMP B':'1010001',
                             }
 
-binario_jump = {'JMP':'1010011',
-                    'JEQ':'1010100',
-                    'JNE':'1010101',
-                    'JGT':'1010110',
-                    'JLT':'1010111',
-                    'JGE':'1011000',
-                    'JLE':'1011001',
-                    'JCR':'1011010',
-                    'JOV':'1011011'}
+binario_jump = {'JMP':'1010011','JEQ':'1010100','JNE':'1010101','JGT':'1010110','JLT':'1010111','JGE':'1011000','JLE':'1011001','JCR':'1011010','JOV':'1011011'}
 
-hexadecimal = {'0E':'00001110',
-               '0D':'00001101',
-               '0C':'00001100',
-               'F2':'11110010'}
+hexadecimal = {'0E':'00001110','0D':'00001101','0C':'00001100','F2':'11110010'}
 
 
 filecheck = True
@@ -143,13 +60,13 @@ memo = 0
 for i in Data:
     if " " not in i:
         filecheck = False
-        print("Debe entregar un valor a la variable")
+        print("Error en la linea: " + i + "\n" + "Debe entregar un valor a la variable", i)
     else:
         i = i.split()
         variables.append(i[0])
         a = i[1]
         if a.count("#") > 1:
-            print("Direccion invalida")
+            print("Error en la linea: " + i[0]+ " " + i[1] + "\n" + i[1] + " No es una direccion valida")
             continue
         elif "#" in a:
             a = a.strip("#")
@@ -157,16 +74,16 @@ for i in Data:
         a = "{0:08b}".format(int(a))
         file2.write(a+"\n")
 file2.close()
-print(Data)
-print(Code)
-print(variables)
+# print(Data)
+# print(Code)
+# print(variables)
 test_variables = []
 for idx, linea in enumerate(Code):
     line = linea.split()
     linea = linea.strip()
     if " " not in linea:
         if linea[-1] != ":":
-            print("Error de escritura")
+            print(linea," Tiene un error de escritura")
             continue
         else:
             direcciones[linea.strip(":")] = memo
@@ -235,7 +152,9 @@ for idx, linea in enumerate(Code):
                     b = linea.index(")")
                     a = linea[a:b]
                     test_variables.append(a)
-                    continue
+                    # print ("Linea: ", line)
+                    # print("AQUIII  ", a)
+                    continue            
             if num > 256:
                 print("Error en la linea {}: {} \tEl literal excede el valor aceptado.".format(idx + 1, linea))
                 filecheck = False
@@ -248,10 +167,29 @@ for idx, linea in enumerate(Code):
             if secondoperand[0] == "#":
                 num = int("0x" + secondoperand.strip("#\n"), 0)
             else:
-                num = int(secondoperand, 0)
+                try:
+                    num = int(secondoperand, 0)
+                except:
+                    a = linea.index("(") + 1
+                    b = linea.index(")")
+                    a = linea[a:b]
+                    test_variables.append(a)
+                    continue
             if num > 256:
                 print("Error en la linea {}: {} \tEl literal excede el valor aceptado.".format(idx + 1, linea))
                 filecheck = False
+        if secondoperand[0] == "(" and line[1] not in listaC:
+            try:
+                num = int(secondoperand, 0)
+            except:
+                a = linea.index("(") + 1
+                b = linea.index(")")
+                a = linea[a:b]
+                test_variables.append(a)
+                # print ("Linea: ", line)
+                # print("AQUIII  ", a)
+                continue
+            
                 
     if ((operation in notDoubles) and (secondoperand[0] == "")):
         print("Error en la linea {}: {} \t{} debe recibir 2 parametros.".format(idx + 1, linea, line[0]))
@@ -350,11 +288,6 @@ for idx, linea in enumerate(Code):
         except:
             pass
 
-    # if operation == "INC" or operation == "RST":
-    #     if firstoperand[0] != "(" or firstoperand == "(A)" or firstoperand in numeros or firstoperand == "A":
-    #         print("Error en la linea {}: {} \tOperacion no soportada.".format(idx + 1, linea, line[0]))
-    #         filecheck = False
-
     if operation == "CMP":
         if firstoperand[0] == "(" or secondoperand == "A" or line[1] in listaMov :
             print("Error en la linea {}: {} \tOperacion no soportada.".format(idx + 1, linea, line[0]))
@@ -450,7 +383,6 @@ if filecheck:
 
                 print("\tSi esta")
 
-                # a = line[1].strip("#")
                 try:
                     a = direcciones[lineaa[1]]
                     a = "{0:08b}".format(int(a))
@@ -470,18 +402,6 @@ if filecheck:
 
                 except:
                     file1.write(binario_direccionamiento[lineaa[0]] + str(((hexadecimal[a]))) + "\n")
-                    # Tira error, no tiene definido cuanto es F2
-
-
-
-
-            
-                    
-                    
-                
-
-                
-
 
 file.close()
 file1.close()
