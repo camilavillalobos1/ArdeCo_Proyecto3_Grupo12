65cc1276f9a086e44893564f7adfe36061682cb3
camilavillalobos1 <civillalobos@miuandes.cl>
2020-10-27 11:18:40 -0300

    Menos lineas

--- Proyecto3.py
+++ Proyecto3.py
@@ -4,24 +4,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--file", help="Assembly file to read", default = "p3F_1.ass")
 parser.add_argument("--output", help="Output file written in binary", default = "datos")
 args = parser.parse_args()
-
 try:
     file = open(args.file, 'r') #Abre archivo de texto con assembly
 except IOError:
     print("[!] No se encontro el archivo")
     exit()
-
-
 file1 = open(args.output+".out", 'w')
 file2 = open(args.output+".mem", 'w')
-
 def test():
     a = linea.index("(") + 1
     b = linea.index(")")
     a = linea[a:b]
     test_variables.append(a)
+    if a not in variables:
+        print ("Error")
     return a
-
 Lineas = file.readlines()
 Code = []
 Data = []
@@ -36,38 +33,24 @@ if "CODE:\n" in Lineas:
     Code.pop(0)
 else:
     Code = Lineas[:]
-
-
-instructions = ["CMP", "JEQ", "JMP","JNE", "JGT", "JLT", "JGE", "JLE", "JCR", "JOV" , "MOV", "SUB", "ADD", "AND", "OR",
-                "NOT", "XOR", "SHL", "SHR", "INC", "RST"]
+instructions = ["CMP", "JEQ", "JMP","JNE", "JGT", "JLT", "JGE", "JLE", "JCR", "JOV" , "MOV", "SUB", "ADD", "AND", "OR","NOT", "XOR", "SHL", "SHR", "INC", "RST"]
 instructions1 = ["SHL", "SHR", "INC", "RST"]
 notDoubles = ["MOV", "CMP"]
 listaMov = ["(A),B", "A,(A)", "A,A", "B,B","(B),B"]
 listaC = ["A,(B)", "B,(B)", "B,(A)"]
-
-
-binario = {'MOV A,B' : '0000000','MOV B,A':'0000001','ADD A,B':'0000100','ADD B,A':'0000101','SUB A,B':'0001000','SUB B,A':'0001001','AND A,B':'0001100','AND B,A':'0001101',
-                    'OR A,B':'0010000','OR B,A':'0010001','NOT A,A':'0010100','NOT A,B':'0010101','NOT B,A':'0010110','NOT B,B':'0010111','XOR A,A':'0011000','XOR B,A':'0011001',
-                    'SHL A,A':'0011100','SHL A,B':'0011101','SHL B,A':'0011110','SHL B,B':'0011111','SHR A,A':'0100000','SHR A,B':'0100001','SHR B,A':'0100010','SHR B,B':'0100011',
-                    'INC B':'0100100','CMP A,B':'1001101','MOV A,(B)':'0101001','MOV B,(B)':'0101010','MOV (B),A':'0101011','ADD A,(B)':'0101110','SUB A,(B)':'0110010','AND A,(B)':'0110110',
-                    'OR A,(B)':'0111010','NOT (B)':'0111110','XOR A,(B)':'1000001','SHL (B)':'1000101','SHR (B)':'1001000','CMP A,(B)':'1010010','INC (B)':'1001010','RST (B)':'1001100'
+binario = {'MOV A,B' : '0000000','MOV B,A':'0000001','ADD A,B':'0000100','ADD B,A':'0000101','SUB A,B':'0001000','SUB B,A':'0001001','AND A,B':'0001100','AND B,A':'0001101','OR A,B':'0010000','OR B,A':'0010001','NOT A,A':'0010100','NOT A,B':'0010101','NOT B,A':'0010110','NOT B,B':'0010111','XOR A,A':'0011000','XOR B,A':'0011001',
+                    'SHL A,A':'0011100','SHL A,B':'0011101','SHL B,A':'0011110','SHL B,B':'0011111','SHR A,A':'0100000','SHR A,B':'0100001','SHR B,A':'0100010','SHR B,B':'0100011','INC B':'0100100','CMP A,B':'1001101','MOV A,(B)':'0101001','MOV B,(B)':'0101010','MOV (B),A':'0101011','ADD A,(B)':'0101110','SUB A,(B)':'0110010',
+                    'AND A,(B)':'0110110','OR A,(B)':'0111010','NOT (B)':'0111110','XOR A,(B)':'1000001','SHL (B)':'1000101','SHR (B)':'1001000','CMP A,(B)':'1010010','INC (B)':'1001010','RST (B)':'1001100'
                     }
-
 binario_literal = {'MOV A':'0000010','MOV B':'0000011','ADD A':'0000110','ADD B':'0000111','SUB A':'0001010','SUB B':'0001011','AND A':'0001110','AND B':'0001111',
                     'OR A':'0010010','OR B':'0010011','XOR A':'0011010','XOR B':'0011011','CMP A':'1001110','CMP B':'1001111'
                     }
 
-binario_direccionamiento = {'MOV A':'0100101','MOV B':'0100110','MOV (),A':'0100111','MOV (),B':'0101000','ADD A':'0101100','ADD B':'0101101','ADD':'0101111',
-                            'SUB A':'0110000','SUB B':'0110001','SUB':'0110011','AND A':'0110100','AND B':'0110101','AND':'0110111','OR A':'0111000','OR B':'0111001',
-                            'OR':'0111011','NOT (),A':'0111100','NOT (),B':'0111101','XOR A':'0111111','XOR B':'1000000','XOR ()':'1000010','SHL (),A':'1000011',
-                            'SHL (),B':'1000100','SHR (),A':'1000110','SHR (),B':'1000111','INC ()':'1001001','RST':'1001011','CMP A':'1010000','CMP B':'1010001',
+binario_direccionamiento = {'MOV A':'0100101','MOV B':'0100110','MOV (),A':'0100111','MOV (),B':'0101000','ADD A':'0101100','ADD B':'0101101','ADD':'0101111', 'SUB A':'0110000','SUB B':'0110001','SUB':'0110011','AND A':'0110100','AND B':'0110101','AND':'0110111','OR A':'0111000','OR B':'0111001',
+                            'OR':'0111011','NOT (),A':'0111100','NOT (),B':'0111101','XOR A':'0111111','XOR B':'1000000','XOR ()':'1000010','SHL (),A':'1000011','SHL (),B':'1000100','SHR (),A':'1000110','SHR (),B':'1000111','INC ()':'1001001','RST':'1001011','CMP A':'1010000','CMP B':'1010001',
                             }
-
 binario_jump = {'JMP':'1010011','JEQ':'1010100','JNE':'1010101','JGT':'1010110','JLT':'1010111','JGE':'1011000','JLE':'1011001','JCR':'1011010','JOV':'1011011'}
-
 hexadecimal = {'0E':'00001110','0D':'00001101','0C':'00001100','F2':'11110010'}
-
-
 filecheck = True
 variables = []
 direcciones = {}
@@ -281,12 +264,10 @@ for idx, linea in enumerate(Code):
             filecheck = False
 
     if line[0] == "CMP":
+        
         if line[1] == "B,(A)" or line[1] == "B,A":
             print("Error en la linea {}: {} \tOperacion no soportada".format(idx + 1, linea,line[0]))
             filecheck = False
-
-
-    if operation == "CMP":
         if firstoperand[0] == "(" or secondoperand == "A" or line[1] in listaMov :
             print("Error en la linea {}: {} \tOperacion no soportada.".format(idx + 1, linea, line[0]))
             filecheck = False
@@ -306,7 +287,6 @@ if filecheck:
 
         if " " not in linea and linea[-1] == ":":
             continue
-        
         linea = linea.strip()
         line = linea.split(",")
         lineaa = linea.split()
